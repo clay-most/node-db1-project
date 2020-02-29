795da51725fa12fb8f153d557a78c9af0bea3256
Anna Morris <annapatton444@gmail.com>
2020-02-29 10:06:25 -0600

    mvp

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const server = require('./server.js');
+const server = require("./server.js");
+const accountsRouter = require("./accountsRouter");
 
 const PORT = process.env.PORT || 4000;
 
+server.use("/", accountsRouter);
+
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`Listening at http://localhost:${PORT}...`);
 });
